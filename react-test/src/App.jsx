667aa6d1685ac244bffa071fae3b2056b719fcc6
Aviv <aviv.dror@gmail.com>
2024-07-18 21:32:11 +0200

    router 3

--- react-test/src/App.jsx
+++ react-test/src/App.jsx
@@ -1,33 +1,35 @@
-import "./App.css";
-import { Link, Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/home-page/HomePage";
-import { AboutPage } from "./pages/about-page/AboutPage";
-import { ContactPage } from "./pages/contact-page/ContactPage";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">home</Link>
-          </li>
-          <li>
-            <Link to="/about">about</Link>
-          </li>
-          <li>
-            <Link to="/contact">contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
       <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <footer>footer</footer>
-    </div>
-  );
-};
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
